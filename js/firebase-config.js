3fd3ae0d9821289b73ab4256a689e13dfd6f127a
RekCutPleh <96849595+Shinninng@users.noreply.github.com>
2025-05-13 21:44:08 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,16 +3,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Configuración directa (solución temporal para desarrollo)
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: "AIzaSyDFgYZwc7gZuBfombUYQ9mMVa6gfkJVGs4",
+  authDomain: "gestordegastos-afff6.firebaseapp.com",
   databaseURL: "https://gestordegastos-afff6-default-rtdb.firebaseio.com",
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+  projectId: "gestordegastos-afff6",
+  storageBucket: "gestordegastos-afff6.firebasestorage.app",
+  messagingSenderId: "993733253098",
+  appId: "1:993733253098:web:070c9b105504d13f818493",
+  measurementId: "G-JV0VLQ3P5Y"
 };
 
 const app = initializeApp(firebaseConfig);
